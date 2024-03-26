407422cb5070dcbe2c0d9ca42068d06b0e99af6d
hixion <mscuderi855@gmail.com>
2024-03-26 07:39:34 -0300

    Improve screenshot crate execution with format options for images

--- screenshot/src/main.rs
+++ screenshot/src/main.rs
@@ -1,6 +1,30 @@
+use std::io;
 mod screenshot;
 
+fn read_format() -> Result<String, String> {
+    println!("Choose the screenshot's format [png | bmp]: ");
+    let mut format = String::new();
+    io::stdin()
+        .read_line(&mut format)
+        .map_err(|err| err.to_string())?;
+
+    Ok(format)
+}
+
+fn return_extension(format: &str) -> Result<String, String> {
+    let format = format.strip_suffix("\r\n").or(format.strip_suffix("\n"));
+
+    let extension = match format {
+        Some(f) if f == "png" || f == "bmp" => format!("screenshot.{}", f),
+        _ => return Err("Unknown or unsopported format".to_string()),
+    };
+
+    Ok(extension)
+}
+
 fn main() -> Result<(), String> {
-    screenshot::screenshot()?;
+    let format = read_format()?;
+    let extension = return_extension(&format)?;
+    screenshot::screenshot(&extension)?;
     Ok(())
 }
